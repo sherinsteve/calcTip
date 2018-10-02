87e1766d92688589b2b8b71aa694d98e6e04af31
Shoja George <shoja@dhcp-v024-202.mobile.uci.edu>
2018-10-01 17:43:00 -0700

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Shoja George on 10/1/18.
+//  Copyright © 2018 Sherin Stephen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
